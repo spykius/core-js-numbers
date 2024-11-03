e9cfb98035675ad8fedf5959fc1f05cb6cc944f5
Nikita Korp <betaquarius203@gmail.com>
2024-11-03 18:30:45 +0300

    feat: implement getSumOfDigits function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -295,8 +295,10 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return String(num)
+    .split('')
+    .reduce((sum, val) => sum + +val, 0);
 }
 
 /**
