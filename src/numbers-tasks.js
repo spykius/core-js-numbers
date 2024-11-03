a89440ef1b6639872a528af8bc8928a579f1864f
Nikita Korp <betaquarius203@gmail.com>
2024-11-03 18:45:27 +0300

    feat: implement toExponential function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -355,10 +355,9 @@ function numberToStringInBase(number, base) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits);
 }
-
 /**
  * Returns a string representation of a number in fixed-point notation.
  *
